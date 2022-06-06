194417ce7b841256a3789915d02359544d3be0be
JonathanHartsell <105687447+JonathanHartsell@users.noreply.github.com>
2022-06-06 17:11:37 -0500

    bagwithlist

---
+++ StarterCode/include/bagwithlist.h
@@ -0,0 +1,41 @@
+//Jonathan Hartsell
+// Assignment 2 Problem 2
+
+#pragma once
+#include <iostream>
+#include "list.h"
+
+template <typename Thing>
+class Bag {
+public:
+  void insert(Thing aThing) {
+    bagContents.push_back(aThing);
+    bagSize++;
+  }
+  Thing &pop() {
+    Thing aThing;
+    if (bagContents.size() > 0) {
+      aThing = bagContents[bagSize];
+      bagSize--;
+    }
+    else {
+      std::cerr << "Can't pop out of an empty bag" << std::endl;
+    }
+    return aThing;
+  }
+  int size() {
+    return bagSize;
+  }
+  int count(Thing aThing) {
+    int bagCount = 0;
+    for (int i = 0 ; i < bagSize; i++) {
+      if (bagContents[i] == aThing) {
+        bagCount++;
+      }
+    }
+    return bagCount;
+  }
+private:
+  vector<Thing> bagContents;
+  int bagSize = 0;
+};
